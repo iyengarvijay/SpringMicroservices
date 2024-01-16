d5d5def48ae2e4d8b537741db3a42c432a0c507b
VIJAY <iyengarvijay741@gmail.com>
2024-01-16 18:50:02 -0500

    Initial Commit includes product and order service microservices

---
+++ product-service/src/main/java/com/technogeek/productservice/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.technogeek.productservice.controller;
+
+import com.technogeek.productservice.dto.ProductRequest;
+import com.technogeek.productservice.dto.ProductResponse;
+import com.technogeek.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
